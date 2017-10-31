7f7e5baf59ceb51075e394143421d6432bba1f3a
Matthieu Sozeau <mattam@mattam.org>
2017-10-31 20:17:43 +0100

    Add script to update the www/ directory on coq.inria.fr

---
+++ update_opam_git.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export PATH=/usr/local/bin/:$PATH
+
+git remote update 1>/dev/null
+
+LOCAL=$(git rev-parse @{0})
+REMOTE=$(git rev-parse @{u})
+
+if [ $LOCAL != $REMOTE ]; then
+    git pull
+    PATH="/usr/local/bin:$PATH" make COQWEB=/srv/coq-www/www all
+fi
